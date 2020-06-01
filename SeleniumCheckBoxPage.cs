4f949ff0a871965282e0eb160aaad017a3cca792
GitanaKurbanove <gitana.kurbanove@gmail.com>
2020-06-01 15:00:59 +0300

    Add files via upload

---
+++ SeleniumCheckBoxPage.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VcsWebdriver.Pages
+{
+    class SeleniumCheckBoxPage : PageBase
+
+    {
+        private IWebElement varnelesLaukas => Driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement varnelesLaukoRezultatas => Driver.FindElement(By.Id("txtAge"));
+        private ReadOnlyCollection<IWebElement> varnelesLaukai => Driver.FindElements(By.XPath("//*[@id='easycont']/div/div[2]/div[2]/div[2]/div/label/input"));
+        private IWebElement mygtukolangelis => Driver.FindElement(By.Id("check1"));
+        
+        public SeleniumCheckBoxPage(IWebDriver inputDriver) : base(inputDriver)
+        {
+            Driver.Url = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html";
+        }
+
+        public SeleniumCheckBoxPage VarnelesLaukas(bool pazymeta)
+        {
+            if (varnelesLaukas.Selected != pazymeta)
+                varnelesLaukas.Click();
+
+            return this;
+        }
+        public void TikrintiRezultata(string tiketinasUzrasas)
+        {
+            string zinute = varnelesLaukoRezultatas.Text;
+            Assert.AreEqual(tiketinasUzrasas, zinute, "Uzrasas nesutrampa");
+            varnelesLaukas.Click();
+        }
+
+        public SeleniumCheckBoxPage VarnelesLaukai(bool pazymeti)
+        {
+            foreach (var laukas in varnelesLaukai)
+            {
+                if (laukas.Selected != pazymeti)
+                    laukas.Click();
+            }
+            return this;
+        }
+       
+
+        public void PatikrinameRezultata(string tiketinasUzrasas)
+        {
+            
+            string message = mygtukolangelis.GetAttribute("value");
+            Assert.AreEqual(tiketinasUzrasas, message, "Uzrasas nesutampa");
+        }
+
+
+        public SeleniumCheckBoxPage AtzymetiLaukai(bool atzymetiLaukai)
+        {
+            mygtukolangelis.Click();
+            foreach (var laukas in varnelesLaukai)
+            {
+                if (laukas.Selected != atzymetiLaukai)
+                    laukas.Click();
+            }
+            Assert.False(atzymetiLaukai, "Laukai neatzymeti");
+
+            return this;
+        }
+
+
+    }
+}
---
+++ SeleniumCheckBoxPageObject.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VcsWebdriver.Pages;
+
+namespace VcsWebdriver.Tests
+{
+    class SeleniumCheckBoxPageObject 
+    {
+        public static SeleniumCheckBoxPage _driver;
+
+        [OneTimeSetUp]
+        public static void SetUpChrome()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            _driver = new SeleniumCheckBoxPage(driver);
+        }
+        [Order(1)]
+        [TestCase(true, "Success - Check box is checked", TestName = "Pazymime varnele ir patikriname uzrasa")]
+        public static void EnterMessageTest(bool pazymeta, string tiketinasUzrasas)
+        {
+            _driver
+                .VarnelesLaukas(pazymeta)
+                .TikrintiRezultata(tiketinasUzrasas);
+        }
+        [Order(2)]
+        [TestCase(true, "Uncheck All", TestName = "Pazymime visas varneles, patikriname kad mygtukas pakeicia pavadinima")]
+
+        public static void PazymetiVisasVarneles(bool pazymeti, string tiketinasUzrasas)
+        {
+            _driver
+                .VarnelesLaukai(pazymeti)
+                .PatikrinameRezultata(tiketinasUzrasas);
+        }
+
+        [Order(3)]
+        [TestCase(false, TestName = "Paslaudziame Uncheck all, patikriname kad visos varneles atzymetos")]
+        public static void PatikrintiKadVarnelesAtzymetos(bool atzymetiLaukai)
+        {
+            _driver
+                .AtzymetiLaukai(atzymetiLaukai);
+                
+        }
+
+
+
+        [OneTimeTearDown]
+        public static void CloseBrowser()
+        {
+            _driver.CloseBrowser();
+        }
+
+    }
+}
